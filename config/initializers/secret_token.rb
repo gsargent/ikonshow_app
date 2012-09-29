b215c89cd62f8e4d539ccbdce61bd6aefa22298b
George Sargent <George.Sargent@gmail.com>
2012-09-28 22:44:19 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ikonicx2::Application.config.secret_token = 'b511eb1583608c88cd12229922bb6b07c095d41620f9bd16f4a7d2e5da87fa99256ced01a1ea9346f323068f8db0ac6e2c0d113849a4bc010d0bc6cb26a201a0'
