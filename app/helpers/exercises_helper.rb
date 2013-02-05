28a90f5297e347f4df81961b42a01cdeacd8cec9
George Sargent <George.Sargent@gmail.com>
2013-02-05 14:12:21 -0600

    Implemented sorting the skills dropdown (exerceses/_form).
    Created a exercises helper method to sort the tags being
    generated for the skills select (drop down) tag.

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -9,11 +9,15 @@ module ExercisesHelper
 	#   from its parent object.
 
 	def get_existing_value_else_current_user_id
-		@exercise.user_id.nil? ? current_user.id : @exercise.user_id
+	  @exercise.user_id.nil? ? current_user.id : @exercise.user_id
 	end
 
 	def get_existing_value_else_current_username
+	  @exercise.user_id.nil? ? current_user.username : @exercise.user.username
+	end
 
-		@exercise.user_id.nil? ? current_user.username : @exercise.user.username
+	def get_sorted_options_tags_for_skills
+	  options_from_collection_for_select(LookupSkill.order(:sort_order), 
+	  	:id, :skillname, @exercise.lookup_skill_id )
 	end
 end
