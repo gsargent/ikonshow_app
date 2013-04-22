bb4767ab6e14520d47608102b11646c709c5eb83
George Sargent <George.Sargent@gmail.com>
2013-04-21 21:52:19 -0500

    Moved User Profile Cancel my account function to accordion
    The Cancel my account always showed on the user's profile page,
    i.e. registrations. Since this is an out of the ordinary function
    I put it into an accordion panel and left it hidden forcing
    the user to open it to get at the cancel functionality.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,13 +3,13 @@ class RegistrationsController < Devise::RegistrationsController
 
   # GET /exercises/1/edit
   def edit 
-    puts "7777777777"
+    puts "EDIT7777777777"
     ap current_user
   end
 
   # POST /users
   def update
-
+    puts "UPDATE88888888"
     # note: if password was not changed it does not need to be set, remove
     #       passwords from the params
     if params[:user][:password].length == 0 then
@@ -21,6 +21,14 @@ class RegistrationsController < Devise::RegistrationsController
     ap current_user
     ap current_user.roles
     puts "=============="
+
+    # test that user didn't hack roles. The roles that went out should be the roles that came in
+    @user = User.find current_user.id
+    old_role_ids = @user.roles.pluck(:id)
+    puts "roles0: #{old_role_ids}"
+    new_role_ids = params["user"]["role_ids"]
+    puts "roles1: #{new_role_ids}"
+    puts "-----done 0 --------"
     super
  
   end
